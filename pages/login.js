22e024a6c7f3341884071e8d38e9c13072d9cdd5
norasaett <norasaett@gmail.com>
2023-03-14 09:26:18 +0700

    1 file modified

--- pages/login.js
+++ pages/login.js
@@ -2,8 +2,24 @@ import DefaultLayout from "@/components/layouts/Default";
 import { useState } from "react";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("saklism+demo1@gmail.com");
+  const [password, setPassword] = useState("12341234");
+  const handleLogin = async() =>{const url = "https://sakko-demo-api.herokuapp.com/api/v1/user/sign_in"
+  const response = await fetch(url,{
+    method: "POST",
+    headers:{
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      user: {
+        email,
+        password,
+      },
+    }),
+  });
+  const json = await response.json();
+  console.log("JSON",json);
+  };
   return (
     <DefaultLayout>
       <div>
